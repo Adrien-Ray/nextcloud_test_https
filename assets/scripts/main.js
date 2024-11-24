54938c22873123964a811f8a8632b4ff2610b63e
Adrien-Ray <forpub@accesdenied.net>
2024-11-24 15:52:50 +0100

    #6 - js create scope onclick if mobile

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+function closeNavMenuMobile() {
+    // cette fonction referme le menu mobile au click sur un item
+    elementsItems = document.querySelectorAll('a.nav__child');
+    console.log(elementsItems);
+    for (let index = 0; index < elementsItems.length; index++) {
+        const element = elementsItems[index];
+        element.addEventListener('click', () => {
+            if (window.innerWidth <= 560) {
+                console.log("Le viewport est inférieur ou égal à 35rem.");
+                console.log(element);
+            } else {
+                console.log("Le viewport est supérieur à 35rem.");
+            }
+            
+        })
+    }
+}
